d54630d2cb4255ea953909fb6d5f84b283927f22
Aurailus <100Toby1@gmail.com>
2017-12-25 12:38:32 -0800

    Refactor all code, NEI like sidebar, registration code for addons

---
+++ ainv_homes/init.lua
@@ -0,0 +1,57 @@
+ainv_homes = {}
+
+local path = minetest.get_modpath("ainv_homes")
+
+dofile(path .. "/formspecs/formspec_homes.lua")
+dofile(path .. "/formspecs/formspec_addhome.lua")
+
+ainv.register_inventory_screen("homes", ainv_homes.gen_formspec_homes, {
+	name = "Homes",
+	image = "auriinventory_tab_icon_10.png",
+	image_hover = "auriinventory_tab_icon_11.png"
+})
+
+ainv.register_callback("homes", function (player, fields)
+	for i = 1, 6 do
+		if fields["home" .. tostring(i)] ~= nil then
+			local hashome = (player:get_attribute("home_" .. i .. "_pos") ~= nil)
+			if not hashome then
+				player:set_attribute("home_editing", i)
+				minetest.show_formspec(player:get_player_name(), "addhome", ainv_homes.gen_formspec_addhome(player, tostring(i)))
+			else
+				player:setpos(minetest.deserialize(player:get_attribute("home_" .. i .. "_pos")))
+			end
+			print(i)
+			return
+		elseif fields["delhome" .. tostring(i)] ~= nil then
+			player:set_attribute("home_" .. i .. "_pos", nil)
+			player:set_attribute("home_" .. i .. "_name", nil)
+			ainv.reloadInventory(player)
+			return
+		end
+	end
+end)
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname == "addhome" then
+		if fields.homename ~= nil then
+			player:set_attribute("home_" .. player:get_attribute("home_editing") .. "_pos", minetest.serialize(player:getpos()))
+			player:set_attribute("home_" .. player:get_attribute("home_editing") .. "_name", fields.homename)
+		end
+		ainv.reloadInventory(player)
+		return
+	end
+end)
+
+minetest.register_chatcommand("setmaxhomes", {
+	params = "<amount>",
+	description = "Set the maximum allowed homes.",
+	privs = {server},
+	func = function (name, param)
+		if tonumber(param) then
+			minetest.setting_set("maxhomes", tonumber(param))
+		else
+			minetest.chat_send_player(name, "Please supply a number.")
+		end
+	end,
+})
